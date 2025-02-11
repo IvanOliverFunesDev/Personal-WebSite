b108f0ceef54731717d5e1f35a6ac94c33a0ff54
Ivan-Adaits <ioliverfunes@adaits.es>
2025-02-11 18:53:37 +0100

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,95 +44,89 @@ export default function Home() {
           filter: isLoaded ? (isVisible ? "blur(0px)" : "blur(10px)") : "blur(10px)",
         }}
         transition={{ duration: 1, ease: "easeOut" }}
-        className="relative flex min-h-screen items-center justify-center bg-[#1E1E2E] text-white"
+        className="relative flex flex-col-reverse md:flex-row min-h-screen items-center justify-center bg-[#1E1E2E] text-white px-6 md:px-10"
       >
-        {/* Contenido principal con la imagen a la derecha y el texto a la izquierda */}
+        {/* Sección de texto */}
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: isLoaded ? 1 : 0 }}
-          transition={{ duration: 1, ease: "easeOut" }}
-          className={`w-full max-w-6xl px-10 flex items-center justify-between ${!isLoaded && "hidden"}`}
-        >
-          {/* Sección de texto con animación desde la izquierda */}
-          <motion.div
-            initial={{ opacity: 0, x: -200 }}
-            animate={{ opacity: isLoaded ? 1 : 0, x: isLoaded ? 0 : -200 }}
-            transition={{ duration: 1.5, ease: "easeOut" }}
-            className="flex flex-col space-y-4 mr-10 max-w-[450px]"
+          initial={{ opacity: 0, x: -200 }}
+          animate={{ opacity: isLoaded ? 1 : 0, x: isLoaded ? 0 : -200 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+          className="flex flex-col space-y-4 text-center md:text-left max-w-[450px] md:mr-16 mt-14 md:mt-0"
           >
-            <h1 className="text-6xl font-bold">
-              Hola, soy <br />
-              <TypewriterEffect />
-            </h1>
-            <p className="text-lg text-gray-300">Bienvenido a mi portafolio.</p>
-              {/* Botón para descargar CV */}
-                      <motion.a
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 0 : 20 }}
-                        transition={{ duration: 1, delay: 1.5, ease: "easeOut" }}
-                        href="/IvanOliverFunesCv.pdf"
-                        download="IvanOliverFunesCv"
-                        className="inline-flex items-center px-6 py-3 bg-[#3498DB] text-white rounded-lg hover:bg-[#2980B9] transition"
-                      >
-                        <FaDownload className="mr-2" />
-                        Descargar CV
-                      </motion.a>
-          </motion.div>
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Hola, soy <br />
+            <TypewriterEffect />
+          </h1>
+          <p className="text-lg text-gray-300">Bienvenido a mi portafolio.</p>
 
-          {/* Imagen con animación creativa */}
-          <motion.div
-            initial={{ opacity: 0, x: 200, rotate: 0 }}
-            animate={{ opacity: isLoaded ? 1 : 0, x: isLoaded ? 0 : 200, rotate: isLoaded ? 5 : 0 }}
-            transition={{ duration: 1.5, delay: 0.5, ease: "easeOut" }}
-            whileHover={{ scale: 1.05, rotate: 0 }}
-            className="relative w-[400px] h-[400px]"
+          {/* Botón de descargar CV */}
+          <motion.a
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 0 : 20 }}
+            transition={{ duration: 1, delay: 1.5, ease: "easeOut" }}
+            href="/IvanOliverFunesCv.pdf"
+            download="IvanOliverFunesCv"
+            className="inline-flex items-center px-6 py-3 bg-[#3498DB] text-white rounded-lg hover:bg-[#2980B9] transition"
           >
-            <Image
-              src="/perfil.png"
-              alt="Perfil"
-              width={400}
-              height={400}
-              className="rounded-lg shadow-lg opacity-90"
-              priority
-            />
-          </motion.div>
+            <FaDownload className="mr-2" />
+            Descargar CV
+          </motion.a>
         </motion.div>
 
-        {/* Redes sociales en la parte inferior izquierda con animación desde abajo */}
+        {/* Imagen con animación creativa */}
         <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: isLoaded ? 1 : 0, y: isLoaded ? 0 : 100 }}
-          transition={{ duration: 1.5, delay: 1, ease: "easeOut" }}
-          className="absolute bottom-10 left-10 flex space-x-4"
+          initial={{ opacity: 0, x: 200, rotate: 0 }}
+          animate={{ opacity: isLoaded ? 1 : 0, x: isLoaded ? 0 : 200, rotate: isLoaded ? 5 : 0 }}
+          transition={{ duration: 1.5, delay: 0.5, ease: "easeOut" }}
+          whileHover={{ scale: 1.05, rotate: 0 }}
+          className="relative w-[250px] h-[250px] md:w-[400px] md:h-[400px] mb-8 md:mb-0"
         >
-          {/* GitHub con efecto 3D */}
-          <motion.a
-            href="https://github.com/IvanOliverFunesDev"
-            target="_blank"
-            className="relative"
-            whileHover={{ scale: 1.2, rotateY: 15 }}
-            whileTap={{ scale: 0.9, rotateY: -15 }}
-            transition={{ duration: 0.3 }}
-          >
-            <FaGithub className="text-4xl text-gray-300 hover:text-blue-500 transition" />
-          </motion.a>
-
-          {/* LinkedIn con efecto 3D */}
-          <motion.a
-            href="https://www.linkedin.com/in/ivan-oliver-funes-a2b686348/"
-            target="_blank"
-            className="relative"
-            whileHover={{ scale: 1.2, rotateY: 15 }}
-            whileTap={{ scale: 0.9, rotateY: -15 }}
-            transition={{ duration: 0.3 }}
-          >
-            <FaLinkedin className="text-4xl text-gray-300 hover:text-blue-500 transition" />
-          </motion.a>
+          <Image
+            src="/perfil.png"
+            alt="Perfil"
+            width={400}
+            height={400}
+            className="rounded-lg shadow-lg opacity-90"
+            priority
+          />
         </motion.div>
       </motion.main>
 
+      {/* Redes sociales en la parte inferior izquierda con animación desde abajo */}
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: isLoaded ? 1 : 0, y: isLoaded ? 0 : 100 }}
+        transition={{ duration: 1.5, delay: 1, ease: "easeOut" }}
+        className="absolute bottom-10 left-10 flex space-x-4"
+      >
+        {/* GitHub con efecto 3D */}
+        <motion.a
+          href="https://github.com/IvanOliverFunesDev"
+          target="_blank"
+          className="relative"
+          whileHover={{ scale: 1.2, rotateY: 15 }}
+          whileTap={{ scale: 0.9, rotateY: -15 }}
+          transition={{ duration: 0.3 }}
+        >
+          <FaGithub className="text-4xl text-gray-300 hover:text-blue-500 transition" />
+        </motion.a>
+
+        {/* LinkedIn con efecto 3D */}
+        <motion.a
+          href="https://www.linkedin.com/in/ivan-oliver-funes-a2b686348/"
+          target="_blank"
+          className="relative"
+          whileHover={{ scale: 1.2, rotateY: 15 }}
+          whileTap={{ scale: 0.9, rotateY: -15 }}
+          transition={{ duration: 0.3 }}
+        >
+          <FaLinkedin className="text-4xl text-gray-300 hover:text-blue-500 transition" />
+        </motion.a>
+      </motion.div>
+
+      {/* Secciones del portafolio */}
       <Education />
-        <Skills />
+      <Skills />
       <Projects />
       <AboutMe />
     </>
